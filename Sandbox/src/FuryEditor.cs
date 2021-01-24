6b70e39c2099ea8c3706fae8da8b8ec48ac9b395
Beckam White <beckam2908@gmail.com>
2021-01-25 02:57:59 +1100

    Fixed entry point

---
+++ Sandbox/src/FuryEditor.cs
@@ -0,0 +1,20 @@
+﻿using Fury;
+
+using System;
+using System.Threading;
+
+namespace FuryEditor
+{
+    public class FuryEditor : Application
+    {
+
+        static void Main(string[] args)
+        {
+            var app = EntryPoint.CreateApplication(new FuryEditor());
+            app.PushLayer(new EditorLayer());
+            app.Run();
+        }
+    }
+
+
+}
